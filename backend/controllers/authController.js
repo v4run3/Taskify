0c66bf5106e195aac40e0ee7596e5be2c3f5c9e2
Varun <varun31201@gmail.com>
2025-10-21 16:54:07 +0530

    Commit
    ADMIN Verification

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -10,21 +10,79 @@ const generateToken = (userId) => {
 //@desc Register a new user
 //@route POST /api/auth/register
 //@access Public
-const registerUser = async (req, res) => {};
+const registerUser = async (req, res) => {
+    try {
+        const { name, email, password, profileImageUrl, adminInviteToken } = req.body;
+        
+        // check is user already exists
 
+        const userExists = await User.findOne({ email });
+        if (userExists) {
+            return  res.status(400).json({ message: 'User already exists' });
+        }
+
+        // determine user role (admin or member)
+
+        let role = 'member';
+        if (
+            adminInviteToken &&
+            adminInviteToken === process.env.ADMIN_INVITE_TOKEN
+        ) {
+            role = 'admin';
+        }
+        //hash password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        //create user
+        const user = await User.create({
+            name,
+            email,
+            password: hashedPassword,
+            profileImageUrl,
+            role,
+        });
+
+        //return user data with JWT
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            role: user.role,
+            profileImageUrl: user.profileImageUrl,
+            token: generateToken(user._id),
+        });
+    } catch (error) {
+        res.status(500).json({ message: 'Server error', error: error.message });
+    }
+};
 //@desc Login user
 //@route POST /api/auth/login
 //@access Public
-const loginUser = async (req, res) => {};
-
+const loginUser = async (req, res) => {
+    try { 
+    } catch (error) {
+        res.status(500).json({ message: 'Server error', error: error.message });
+    }
+};
 //@desc Get user profile
 //@route GET /api/auth/profile
 //@access Private
-const getUserProfile = async (req, res) => {};
+const getUserProfile = async (req, res) => {
+    try { 
+    } catch (error) {
+        res.status(500).json({ message: 'Server error', error: error.message });
+    }
+};
 
 //@desc Update user profile
 //@route PUT /api/auth/profile
 //@access Private
-const updateUserProfile = async (req, res) => {};
+const updateUserProfile = async (req, res) => {
+    try { 
+    } catch (error) {
+        res.status(500).json({ message: 'Server error', error: error.message });
+    }
+};
 
 module.exports = {registerUser,loginUser,getUserProfile,updateUserProfile};
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ const cors = require("cors");
 const path = require("path");
 const connectDB = require("./config/db");
 
+const authRoutes = require("./routes/authRoutes");
+
 const app = express();
 
 // Middleware to handle CORS
@@ -22,7 +24,7 @@ connectDB();
 app.use(express.json());
 
 //routes
-// app.use("/api/auth",authRoutes);
+app.use("/api/auth",authRoutes);
 // app.use("/api/users",userRoutes);
 // app.use("/api/tasks",taskRoutes);
 // app.use("/api/reports",reportRoutes);
