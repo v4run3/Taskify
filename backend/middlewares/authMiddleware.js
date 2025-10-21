ed3f8b391902ec4dbd4dd636b5ed03b3808ba7db
Varun <varun31201@gmail.com>
2025-10-21 16:18:18 +0530

    Commit
    Commit on Routes and Middleware for Admin

---
+++ backend/middlewares/authMiddleware.js
@@ -0,0 +1,30 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+const protect = async (req, res, next) => {
+    try {
+        let token = req.headers.authorization;
+
+        if (token && token.startsWith('Bearer ')) {
+            token = token.split(' ')[1];
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+            req.user = await User.findById(decoded.id).select('-password');
+            next();
+        } else {
+            res.status(401).json({ message: 'Not authorized, no token' });
+        }
+    } catch (error) {
+        res.status(401).json({message: 'Token failed',error: error.message});
+    }
+};
+
+//middleware for only admin access
+const adminOnly = (req, res, next) => {
+    if (req.user && req.user.role === 'admin') {
+        next();
+    } else {
+        res.status(403).json({ message: 'Access denied, admin only' });;
+    }
+};
+
+module.exports = { protect, adminOnly };
