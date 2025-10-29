7f5292c71dab133e6751f4f9d68c96c8e506f3ef
Varun <varun31201@gmail.com>
2025-10-29 18:20:12 +0530

    Update Login.jsx
    added error catching mechanism

--- frontend/Taskify/src/pages/Auth/Login.jsx
+++ frontend/Taskify/src/pages/Auth/Login.jsx
@@ -29,9 +29,28 @@ const Login = () => {
     
     //login API call
     try {
+      const response = await axiosInstance.post(API_PATHS.AUTH.LOGIN, {
+        email,
+        password,
+      });
 
+      const { token,role } = response.data;
+
+      if (token) {
+        localStorage.setItem("token", token);
+
+        if (role === "admin") {
+          navigate("/admin/dashboard");
+        } else {
+          navigate("/user/dashboard");
+        }
+      }
     } catch (error) {
-    
+      if (error.response && error.response.data.message) {
+        setError(error.response.data.message);
+      } else {
+        setError("An error occurred. Please try again.");
+      }
     }
   };
 
